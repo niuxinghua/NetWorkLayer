4315ca832bbfa4332fa44ee69212cdafb466cd08
niuxinghua <xinghua5@staff.weibo.com>
2017-02-09 01:38:46 +0800

    修改开源的网络库封装

---
+++ PPNetworkHelper-master/PPNetworkHelper.podspec
@@ -0,0 +1,26 @@
+
+Pod::Spec.new do |s|
+
+  s.name         = "PPNetworkHelper"
+  s.version      = "0.6.0"
+  s.summary      = "对AFNetworking 3.x 与YYCache的二次封装,一句代码搞定数据请求与缓存,告别FMDB!"
+
+  s.homepage     = "https://github.com/jkpang/PPNetworkHelper.git"
+ 
+  s.license      = { :type => "MIT", :file => "LICENSE" }
+
+  s.author       = { "jkpang" => "jkpang@outlook.com" }
+
+  s.platform     = :ios, "7.0"
+
+  s.source       = { :git => "https://github.com/jkpang/PPNetworkHelper.git", :tag => s.version.to_s }
+
+  s.source_files = "PPNetworkHelper/PPNetworkHelper/*.{h,m}"
+  
+  s.dependency 'AFNetworking'
+
+  s.dependency 'YYCache'
+
+  s.requires_arc = true
+
+end
---
+++ PPNetworkHelper-master/PPNetworkHelper/ViewController.m
@@ -0,0 +1,202 @@
+//
+//  ViewController.m
+//  PPNetworkHelper
+//
+//  Created by AndyPang on 16/8/12.
+//  Copyright © 2016年 AndyPang. All rights reserved.
+//
+
+/*
+ *********************************************************************************
+ *
+ *⭐️⭐️⭐️ 新建 PP-iOS学习交流群: 323408051 欢迎加入!!! ⭐️⭐️⭐️
+ *
+ * 如果您在使用 PPNetworkHelper 的过程中出现bug或有更好的建议,还请及时以下列方式联系我,我会及
+ * 时修复bug,解决问题.
+ *
+ * Weibo : CoderPang
+ * Email : jkpang@outlook.com
+ * QQ 群 : 323408051
+ * GitHub: https://github.com/jkpang
+ *
+ * PS:我的另外两个很好用的封装,欢迎使用!
+ * 1.一行代码获取通讯录联系人,并进行A~Z精准排序(已处理姓名所有字符的排序问题):
+ *   GitHub:https://github.com/jkpang/PPGetAddressBook
+ * 2.iOS中一款高度可定制性商品计数按钮(京东/淘宝/饿了么/美团外卖/百度外卖样式):
+ *   GitHub:https://github.com/jkpang/PPNumberButton
+ *
+ * 如果 PPGetAddressBookSwift 好用,希望您能Star支持,你的 ⭐️ 是我持续更新的动力!
+ *********************************************************************************
+ */
+
+#import "ViewController.h"
+#import "NXHNetworkHelper.h"
+
+
+static NSString *const dataUrl = @"http://www.qinto.com/wap/index.php?ctl=article_cate&act=api_app_getarticle_cate&num=1&p=1";
+static NSString *const downloadUrl = @"http://wvideo.spriteapp.cn/video/2016/0328/56f8ec01d9bfe_wpd.mp4";
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *networkData;
+@property (weak, nonatomic) IBOutlet UITextView *cacheData;
+@property (weak, nonatomic) IBOutlet UILabel *cacheStatus;
+@property (weak, nonatomic) IBOutlet UISwitch *cacheSwitch;
+@property (weak, nonatomic) IBOutlet UIProgressView *progress;
+@property (weak, nonatomic) IBOutlet UIButton *downloadBtn;
+
+/** 是否开启缓存*/
+@property (nonatomic, assign, getter=isCache) BOOL cache;
+
+/** 是否开始下载*/
+@property (nonatomic, assign, getter=isDownload) BOOL download;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+   
+    //实时监测网络状态
+       
+    // 一次性获取当前网络状态
+    [self currentNetworkStatus];
+
+}
+
+#pragma  mark - 获取数据
+- (void)getData:(BOOL)isOn url:(NSString *)url
+{
+
+    //自动缓存
+    if(isOn)
+    {
+        self.cacheStatus.text = @"缓存打开";
+        self.cacheSwitch.on = YES;
+        [NXHNetworkHelper GET:url parameters:nil responseCache:^(id responseCache) {
+            
+            self.cacheData.text = [self jsonToString:responseCache];
+            
+        } success:^(id responseObject) {
+            
+            self.networkData.text = [self jsonToString:responseObject];
+            
+        } failure:^(NSError *error) {
+            
+        }];
+        
+    }
+    //无缓存
+    else
+    {
+        self.cacheStatus.text = @"缓存关闭";
+        self.cacheSwitch.on = NO;
+        self.cacheData.text = @"";
+        
+        [NXHNetworkHelper GET:url parameters:nil success:^(id responseObject) {
+            
+            self.networkData.text = [self jsonToString:responseObject];
+            
+        } failure:^(NSError *error) {
+            
+        }];
+        
+    }
+    
+}
+
+#pragma mark - 一次性网络状态判断
+- (void)currentNetworkStatus
+{
+    
+    // 或
+//    if ([PPNetworkHelper isNetwork]) {
+//        NSLog(@"有网络");
+//        if ([PPNetworkHelper isWWANNetwork]) {
+//            NSLog(@"手机网络");
+//        }else if ([PPNetworkHelper isWiFiNetwork]){
+//            NSLog(@"WiFi网络");
+//        }
+//    }
+}
+
+#pragma mark - 下载
+
+- (IBAction)download:(UIButton *)sender {
+    
+    static NSURLSessionTask *task = nil;
+    //开始下载
+    if(!self.isDownload)
+    {
+        self.download = YES;
+        [self.downloadBtn setTitle:@"取消下载" forState:UIControlStateNormal];
+        
+        task = [NXHNetworkHelper downloadWithURL:downloadUrl fileDir:@"Download" progress:^(NSProgress *progress) {
+            
+            CGFloat stauts = 100.f * progress.completedUnitCount/progress.totalUnitCount;
+            self.progress.progress = stauts/100.f;
+            
+            NSLog(@"下载进度 :%.2f%%,,%@",stauts,[NSThread currentThread]);
+        } success:^(NSString *filePath) {
+            
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"下载完成!"
+                                                                message:[NSString stringWithFormat:@"文件路径:%@",filePath]
+                                                               delegate:nil
+                                                      cancelButtonTitle:@"确定"
+                                                      otherButtonTitles:nil];
+            [alertView show];
+            [self.downloadBtn setTitle:@"重新下载" forState:UIControlStateNormal];
+            NSLog(@"filePath = %@",filePath);
+            
+        } failure:^(NSError *error) {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"下载失败"
+                                                                message:[NSString stringWithFormat:@"%@",error]
+                                                               delegate:nil
+                                                      cancelButtonTitle:@"确定"
+                                                      otherButtonTitles:nil];
+            [alertView show];
+            NSLog(@"error = %@",error);
+        }];
+
+    }
+    //暂停下载
+    else
+    {
+        self.download = NO;
+        [task suspend];
+        self.progress.progress = 0;
+        [self.downloadBtn setTitle:@"开始下载" forState:UIControlStateNormal];
+    }
+    
+    
+    
+}
+
+#pragma mark - 缓存开关
+- (IBAction)isCache:(UISwitch *)sender {
+    
+    NSUserDefaults *userDefault = [NSUserDefaults standardUserDefaults];
+    [userDefault setBool:sender.isOn forKey:@"isOn"];
+    [userDefault synchronize];
+    
+    [self getData:sender.isOn url:dataUrl];
+}
+
+/**
+ *  json转字符串
+ */
+- (NSString *)jsonToString:(NSDictionary *)dic
+{
+    if(!dic){
+        return nil;
+    }
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:nil];
+    return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+@end
